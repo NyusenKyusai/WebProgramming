cc4ed2d0a412f3fa5b4cc0f3eb4f3b1246b0ccc3
Jonah <18018535@uhi.ac.uk>
2021-10-03 14:05:35 +0100

    Fixing What the Correct Files Meant

--- Week3/Spritesheets/main.js
+++ Week3/Spritesheets/main.js
@@ -14,12 +14,6 @@ let b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
 let b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
 let b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
 
-/**
- * EaselJS Global Things
- */
-
-let easelCan, easelCTX, loader, stage, stageHeight, stageWidth;
-
 /*****
  * Objects for Destruction
  */
@@ -89,74 +83,7 @@ let changeUserData = (target, property, newValue) => {
 
   target.GetBody().SetUserData(currentData);
 
-  //console.log(target.GetBody().GetUserData());
-};
-
-const tick = (e) => {
-  update();
-  stage.update(e);
-};
-
-const handleComplete = () => {
-  easelGround = makeBitmap(
-    loader.getResult("background"),
-    stageWidth,
-    stageHeight + 140
-  );
-  easelGround.x = 0;
-  easelGround.y = 0;
-
-  easelPost = makeBitmap(loader.getResult("plankv"), 10, 15);
-  easelPost.x = post.GetBody().GetPosition().x * SCALE;
-  easelPost.y = post.GetBody().GetPosition().y * SCALE;
-
-  easelBird = makeBitmap(loader.getResult("bird"), 20, 20);
-
-  stage.addChild(easelGround, easelPost, easelBird);
-
-  for (let i in pigData) {
-    easelPigs.push(
-      makeBitmap(loader.getResult("pig"), pigData[i][5], pigData[i][5])
-    );
-    stage.addChild(easelPigs[easelPigs.length - 1]);
-  }
-
-  plankH = loader.getResult("plankh");
-  plankV = loader.getResult("plankv");
-
-  for (let i in platformData) {
-    let planktype = platformData[i].type == "plankh" ? plankH : plankV;
-    //console.log(planktype);
-
-    easelPlats.push(
-      makeBitmap(planktype, platformData[i].data[5], platformData[i].data[6])
-    );
-
-    stage.addChild(easelPlats[easelPlats.length - 1]);
-  }
-
-  createjs.Ticker.framerate = 60;
-  createjs.Ticker.timingMode = createjs.Ticker.RAF;
-  createjs.Ticker.addEventListener("tick", tick);
-};
-
-/**
- * EaselJS Helpers
- */
-
-const makeBitmap = (loaderImg, b2x, b2y) => {
-  let theImage = new createjs.Bitmap(loaderImg);
-
-  let scaleX = (b2x * 2) / theImage.image.naturalWidth;
-  let scaleY = (b2y * 2) / theImage.image.naturalHeight;
-
-  theImage.scaleX = scaleX;
-  theImage.scaleY = scaleY;
-
-  theImage.regX = theImage.image.width / 2;
-  theImage.regY = theImage.image.height / 2;
-
-  return theImage;
+  console.log(target.GetBody().GetUserData());
 };
 
 /****
@@ -175,37 +102,35 @@ let ground = defineNewBody(
   WIDTH / 2,
   HEIGHT,
   WIDTH / 2,
-  80,
+  5,
   "ground",
   "static"
 );
 
-let post = defineNewBody(1.0, 0.5, 0.1, 70, 510, 10, 15, "post", "static");
+let post = defineNewBody(1.0, 0.5, 0.1, 70, 575, 5, 30, "post", "static");
 
 // Structures
 let platformData = [
-  { data: [1.0, 0.5, 0.1, 600, 485, 5, 70, "plat", "dynamic"], type: "plankv" },
-  { data: [1.0, 0.5, 0.1, 700, 485, 5, 70, "plat", "dynamic"], type: "plankv" },
-  { data: [1.0, 0.5, 0.1, 650, 360, 70, 5, "plat", "dynamic"], type: "plankh" },
-  { data: [1.0, 0.5, 0.1, 650, 305, 5, 50, "plat", "dynamic"], type: "plankv" },
-  { data: [1.0, 0.5, 0.1, 650, 249, 70, 5, "plat", "dynamic"], type: "plankh" },
+  [1.0, 0.5, 0.1, 600, 575, 5, 70, "plat", "dynamic"],
+  [1.0, 0.5, 0.1, 700, 575, 5, 70, "plat", "dynamic"],
+  [1.0, 0.5, 0.1, 650, 450, 70, 5, "plat", "dynamic"],
+  [1.0, 0.5, 0.1, 650, 395, 5, 50, "plat", "dynamic"],
+  [1.0, 0.5, 0.1, 650, 339, 70, 5, "plat", "dynamic"],
 ];
 
 let platforms = [];
 
 platformData.forEach((platform) => {
-  platforms.push(defineNewBody(...platform.data));
+  platforms.push(defineNewBody(...platform));
 });
-
-//console.log(platforms);
 
 // Pigs
 
 let pigData = [
-  [1.0, 0.5, 0.1, 600, 360, 10, "pig"],
-  [1.0, 0.5, 0.1, 700, 360, 10, "pig"],
-  [1.0, 0.5, 0.1, 600, 230, 10, "pig"],
-  [1.0, 0.5, 0.1, 700, 230, 10, "pig"],
+  [1.0, 0.5, 0.1, 600, 430, 10, "pig"],
+  [1.0, 0.5, 0.1, 700, 430, 10, "pig"],
+  [1.0, 0.5, 0.1, 600, 320, 10, "pig"],
+  [1.0, 0.5, 0.1, 700, 320, 10, "pig"],
 ];
 
 let pigs = [];
@@ -217,44 +142,20 @@ pigData.forEach((pig) => {
 
 // Dynamic
 
-let angryBird = defineNewCircle(1.0, 0.5, 0.1, 70, 470, 20, "angrybird");
+let angryBird = defineNewCircle(1.0, 0.5, 0.1, 70, 520, 20, "angrybird");
 
-let easelBird, easelGround, easelPost, plankH, plankV;
-let easelPlats = [];
-let easelPigs = [];
-
-const init = () => {
-  easelCan = document.getElementById("easelcan");
-  easelCTX = easelCan.getContext("2d");
-  stage = new createjs.Stage(easelCan);
-  stage.snapPixelsEnabled = true;
-  stageWidth = stage.canvas.width;
-  stageHeight = stage.canvas.height;
-
-  let manifest = [
-    { src: "background.png", id: "background" },
-    { src: "bird.png", id: "bird" },
-    { src: "pig.png", id: "pig" },
-    { src: "plankh.png", id: "plankh" },
-    { src: "plankv.png", id: "plankv" },
-  ];
-
-  loader = new createjs.LoadQueue(false);
-  loader.addEventListener("complete", handleComplete);
-  loader.loadManifest(manifest, true, "./assets/");
-
-  /*
+/*
 Debug Draw
 */
 
-  let debugDraw = new b2DebugDraw();
-  debugDraw.SetSprite(document.getElementById("b2dcan").getContext("2d"));
-  debugDraw.SetDrawScale(SCALE);
-  debugDraw.SetFillAlpha(0.3);
-  debugDraw.SetLineThickness(1.0);
-  debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
-  world.SetDebugDraw(debugDraw);
-};
+let debugDraw = new b2DebugDraw();
+debugDraw.SetSprite(document.getElementById("b2dcan").getContext("2d"));
+debugDraw.SetDrawScale(SCALE);
+debugDraw.SetFillAlpha(0.3);
+debugDraw.SetLineThickness(1.0);
+debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+world.SetDebugDraw(debugDraw);
+
 // Update World Loop
 
 let update = () => {
@@ -262,43 +163,19 @@ let update = () => {
   world.DrawDebugData();
   world.ClearForces();
 
-  easelBird.x = angryBird.GetBody().GetPosition().x * SCALE;
-  easelBird.y = angryBird.GetBody().GetPosition().y * SCALE;
-  easelBird.rotation = angryBird.GetBody().GetAngle() * (180 / Math.PI);
-
-  for (let i in pigs) {
-    easelPigs[i].x = pigs[i].GetBody().GetPosition().x * SCALE;
-    easelPigs[i].y = pigs[i].GetBody().GetPosition().y * SCALE;
-    easelPigs.rotation = pigs[i].GetBody().GetAngle() * (180 / Math.PI);
-  }
-
-  for (let i in easelPlats) {
-    //console.log(easelPlats[i]);
-    //console.log(platforms[i].GetBody().GetPosition().x * SCALE);
-    easelPlats[i].x = platforms[i].GetBody().GetPosition().x * SCALE;
-    easelPlats[i].y = platforms[i].GetBody().GetPosition().y * SCALE;
-    easelPlats[i].rotation =
-      platforms[i].GetBody().GetAngle() * (180 / Math.PI);
-  }
-
   for (let i in destroyList) {
     if ((destroyList[i].GetUserData().id = "pig")) {
       score += 30;
       $("#score").html(score);
-
-      stage.removeChild(easelPigs[i]);
-      easelPigs.splice(i, 1);
-      pigs.splice(i, 1);
     }
     world.DestroyBody(destroyList[i]);
   }
   destroyList.length = 0;
 
-  //window.requestAnimationFrame(update);
+  window.requestAnimationFrame(update);
 };
 
-//window.requestAnimationFrame(update);
-init();
+window.requestAnimationFrame(update);
 
 /*****
  * Listeners
@@ -355,7 +232,7 @@ world.SetContactListener(listener);
  * Mouse Controls
  */
 
-$("#easelcan").mousedown(function (e) {
+$("#b2dcan").mousedown(function (e) {
   // (x1 - x2)**2 + (y1 - y2)**2 <= radius**2
   let bodyX = angryBird.GetBody().GetPosition().x * SCALE;
   let bodyY = angryBird.GetBody().GetPosition().y * SCALE;
@@ -372,7 +249,7 @@ $("#easelcan").mousedown(function (e) {
   }
 });
 
-$("#easelcan").mouseup(function (e) {
+$("#b2dcan").mouseup(function (e) {
   if (fireBird) {
     let endX = e.offsetX;
     let endY = e.offsetY;
@@ -381,7 +258,7 @@ $("#easelcan").mouseup(function (e) {
     let magnitude = Math.sqrt(((endX - startX) ** 2) + ((endY - startY) ** 2));
     let direction = Math.atan((endY - startY) / (endX - startX));
 
-    //console.log(magnitude);
+    console.log(magnitude);
 
     let xVector = Math.cos(direction) * magnitude;
     if (endX > startX) {
@@ -393,7 +270,8 @@ $("#easelcan").mouseup(function (e) {
     // if (endY < startY) {
     //   yVector = yVector * -1;
     // }
-    //console.log("xVec = " + xVector + " yVec = " + yVector);
+
+    console.log("xVec = " + xVector + " yVec = " + yVector);
 
     angryBird
       .GetBody()
